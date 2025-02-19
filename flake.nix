95e7be92bc9f9518394ba44ec0c2036dbc1f736e
fanshi1028 <jackychany321@gmail.com>
2025-02-19 21:57:06 +0800

    update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
-  # inputs = { nixpkgs.url = "github:Nixos/nixpkgs"; };
-  inputs = { nixpkgs.url = "nixpkgs-unstable"; };
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "nixpkgs-unstable";
+    my-aider.url = "my-aider";
+    my-wg-quick.url = "my-wg-quick";
+  };
+  outputs = { self, nixpkgs, my-aider, my-wg-quick }: {
     devShells = builtins.mapAttrs (system: pkgs:
       let
         # emacs-grammars = (pkgs.callPackage
@@ -13,13 +16,12 @@
       in with pkgs;
       let
         # NOTE: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/data/fonts/nerdfonts/shas.nix
-        nerd-fonts-symbol =
-          nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; };
         fonts = [
           # sarasa-gothic
-          nerd-fonts-symbol
+          nerdfonts.nerd-fonts-symbols-only
         ];
-
+        aider = my-aider.packages.${system}.default;
+        wg-quick = my-wg-quick.packages.${system}.default;
       in {
         default = mkShell {
           buildInputs = [
@@ -37,8 +39,19 @@
             djvulibre
             ffmpegthumbnailer
             mediainfo
-          ] ++ fonts
-            # NOTE: for compiling vterm
+            # NOTE: for jinx-mod.dylib compilation
+            pkg-config
+            (enchant.override {
+              aspell =
+                # NOTE: https://github.com/NixOS/nixpkgs/issues/1000#issuecomment-491513497
+                aspellWithDicts (ds: with ds; [ en ]);
+            })
+            wordnet
+            aider
+            wg-quick
+          ]
+          # ++ fonts
+          # NOTE: for compiling VTERM
             ++ [
               libtool
               cmake
@@ -46,19 +59,19 @@
             # NOTE: for doom doctor checking font requirements
             ++ [ fontconfig ];
 
-          # NOTE: remember to delete the fonts hardlinked when it is updated to replace it by the shellHook.
-          shellHook = let font-dest = "~/Library/fonts/nix";
-          in ''
-            if [ ! -d ${font-dest} ]; then
-              mkdir -p ${font-dest}
-            fi
-            if [ ! -d ${font-dest}/NerdFonts ]; then
-              cp -ral ${nerd-fonts-symbol}/share/fonts/truetype/NerdFonts ${font-dest}
-            fi
-            # ln -s {emacs-grammars}/lib ~/.emacs.d/.local/cache/tree-sitter
-            # echo ${tree-sitter-grammars.tree-sitter-typescript}
-            # echo ${tree-sitter-grammars.tree-sitter-tsx}
-          '';
+          # NOTE: remember to delete the fontd hard-linked when it is updated to replace it by the shellHook.
+          # shellHook = let font-dest = "~/Library/fonts/nix";
+          # in ''
+          #   if [ ! -d ${font-dest} ]; then
+          #     mkdir -p ${font-dest}
+          #   fi
+          #   if [ ! -d ${font-dest}/NerdFonts ]; then
+          #     cp -ral ${nerd-fonts-symbol}/share/fonts/truetype/NerdFonts ${font-dest}
+          #   fi
+          #   # ln -s {emacs-grammars}/lib ~/.emacs.d/.local/cache/tree-sitter
+          #   # echo ${tree-sitter-grammars.tree-sitter-typescript}
+          #   # echo ${tree-sitter-grammars.tree-sitter-tsx}
+          # '';
         };
       }) nixpkgs.legacyPackages;
   };

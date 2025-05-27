7f5a980e4f8aee398ebbd3a2a00c99862a0f6197
fanshi1028 <jackychany321@gmail.com>
2025-05-27 18:46:07 +0800

    fix(nix): local nix env

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells = builtins.mapAttrs (
+        system: pkgs: with pkgs; {
+          default = mkShell {
+            buildInputs =
+              [
+                ripgrep
+                findutils
+                fd
+                libvterm-neovim
+                sqlite
+                ledger
+                gnupg
+                pass
+                coreutils
+                pandoc
+                hledger
+                djvulibre
+                ffmpegthumbnailer
+                mediainfo
+                emacs-lsp-booster
+                imagemagick
+                nixfmt-rfc-style
+              ]
+              # NOTE: for compiling vterm
+              ++ [
+                libtool
+                cmake
+              ];
+          };
+        }
+      ) nixpkgs.legacyPackages;
+    };
+}

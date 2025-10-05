6193fa71ff07a55c5d63939ccd5985df4cafb461
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-10-05 14:14:26 +0800

    feat: nix shells for vterm & jinx

--- flake.nix
+++ flake.nix
@@ -7,35 +7,40 @@
     {
       devShells = builtins.mapAttrs (
         system: pkgs: with pkgs; {
+          jinx-compile = mkShell {
+            buildInputs = [ enchant ];
+            nativeBuildInputs = [ pkg-config ];
+          };
+          vterm-compile = mkShell {
+            buildInputs = [ libvterm-neovim ];
+            nativeBuildInputs = [
+              cmake
+              libtool
+            ];
+          };
           default = mkShell {
-            buildInputs =
-              [
-                ripgrep
-                findutils
-                fd
-                libvterm-neovim
-                sqlite
-                ledger
-                gnupg
-                pass
-                coreutils
-                pandoc
-                hledger
-                djvulibre
-                ffmpegthumbnailer
-                mediainfo
-                emacs-lsp-booster
-                imagemagick
-                nixfmt-rfc-style
-                vips
-                epub-thumbnailer
-                poppler-utils
-              ]
-              # NOTE: for compiling vterm
-              ++ [
-                libtool
-                cmake
-              ];
+            buildInputs = [
+              ripgrep
+              findutils
+              fd
+              libvterm-neovim
+              sqlite
+              ledger
+              gnupg
+              pass
+              coreutils
+              pandoc
+              hledger
+              djvulibre
+              ffmpegthumbnailer
+              mediainfo
+              emacs-lsp-booster
+              imagemagick
+              nixfmt-rfc-style
+              vips
+              epub-thumbnailer
+              poppler-utils
+            ];
           };
         }
       ) nixpkgs.legacyPackages;

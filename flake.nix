494203ad2b6474217b923277e6d1745191aba84b
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-10-05 14:13:46 +0800

    fix: Revert "feat: nix add package vterm-compile"
    This reverts commit 8d534d0f7c0de98d818ef0ec41aa23cfc22fcadc.

--- flake.nix
+++ flake.nix
@@ -5,48 +5,37 @@
   outputs =
     { self, nixpkgs }:
     {
-      packages = builtins.mapAttrs (
-        system: pkgs: with pkgs; {
-          vterm-compile = writeShellApplication {
-            name = "vterm-compile";
-            text = ''
-              ~/.emacs.d/bin/doom env
-              emacs -f vterm-module-compile
-              echo "Done. Please regenerate doom env by running ~/.emacs.d/bin/doom env"
-            '';
-            runtimeInputs = [
-              # emacs -- NOTE HACK: from my global env
-              libtool
-              cmake
-              libvterm-neovim
-            ];
-          };
-        }
-      ) nixpkgs.legacyPackages;
       devShells = builtins.mapAttrs (
         system: pkgs: with pkgs; {
           default = mkShell {
-            buildInputs = [
-              ripgrep
-              findutils
-              fd
-              sqlite
-              ledger
-              gnupg
-              pass
-              coreutils
-              pandoc
-              hledger
-              djvulibre
-              ffmpegthumbnailer
-              mediainfo
-              emacs-lsp-booster
-              imagemagick
-              nixfmt-rfc-style
-              vips
-              epub-thumbnailer
-              poppler-utils
-            ];
+            buildInputs =
+              [
+                ripgrep
+                findutils
+                fd
+                libvterm-neovim
+                sqlite
+                ledger
+                gnupg
+                pass
+                coreutils
+                pandoc
+                hledger
+                djvulibre
+                ffmpegthumbnailer
+                mediainfo
+                emacs-lsp-booster
+                imagemagick
+                nixfmt-rfc-style
+                vips
+                epub-thumbnailer
+                poppler-utils
+              ]
+              # NOTE: for compiling vterm
+              ++ [
+                libtool
+                cmake
+              ];
           };
         }
       ) nixpkgs.legacyPackages;
